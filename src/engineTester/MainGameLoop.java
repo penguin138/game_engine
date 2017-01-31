23564128b2432521f8f23bfdf538de962c07001e
penguin138 <fridaythethirteens@gmail.com>
2017-01-31 10:42:16 +0300

    add completed second tutorial

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -2,6 +2,9 @@ package engineTester;
 
 import org.lwjgl.opengl.Display;
 import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
 
 /**
  * Created by nastya on 17/07/16.
@@ -10,9 +13,28 @@ public class MainGameLoop {
 
     public static void main(String[] args) {
         DisplayManager.createDisplay();
+        Loader loader = new Loader();
+        Renderer renderer = new Renderer();
+
+        float[] vertices = { // first triangle
+                            -0.5f, 0.5f, 0f,
+                             -0.5f, -0.5f, 0f,
+                             0.5f, -0.5f, 0f,
+                             // second triangle
+                             0.5f, -0.5f, 0f,
+                             0.5f, 0.5f, 0f,
+                             -0.5f, 0.5f, 0f};
+
+        RawModel model = loader.loadToVAO(vertices);
+
+
+
         while(!Display.isCloseRequested()) {
             DisplayManager.updateDisplay();
+            renderer.prepare();
+            renderer.render(model);
         }
+        loader.cleanUp();
         DisplayManager.destroyDisplay();
     }
 }
